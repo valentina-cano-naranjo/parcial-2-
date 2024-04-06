93564e898dc1eb1a7b2a95ae12023e91f2234a00
valentina-cano-naranjo <159857592+valentina-cano-naranjo@users.noreply.github.com>
2024-04-05 21:11:15 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+bool Empezar = true;
+Console.WriteLine("Bienvenidos a Adivina el número");
+
+//pedir numero de jugadores//
+int numjugadores = 0;
+
+Console.WriteLine("Ingrese el número de jugadores (entre 2 y 4 ):");
+numjugadores = Convert.ToInt32(Console.ReadLine());
+
+while (!int.TryParse(Console.ReadLine(), out numjugadores) || numjugadores < 2 || numjugadores > 4) ;
+
+int maximo = numjugadores switch
+{
+2 => 50,
+3 => 100,
+4 => 200,
+_ => 100
+};
+
+Random rnd = new Random();
+int numeroAleatorio = rnd.Next(0, maximo + 1);
+
+int jugadorActual = 1;
+
+while (true)
+{
+Console.WriteLine($"\nTurno del jugador {jugadorActual}");
+Console.Write("Ingrese un número: ");
+if (!int.TryParse(Console.ReadLine(), out int numeroIngresado))
+{
+Console.WriteLine("Por favor ingrese un número válido.");
+continue;
+}
+
+if (numeroIngresado < numeroAleatorio)
+{
+Console.WriteLine("MAYOR");
+}
+else if (numeroIngresado > numeroAleatorio)
+{
+Console.WriteLine("MENOR");
+}
+else
+{
+Console.WriteLine("¡HAS GANADO!");
+break;
+}
+
+jugadorActual = (jugadorActual % numjugadores) + 1;
+}
+
+Console.Write("¿Desean jugar otra vez? (Sí/No): ");
+Empezar = Console.ReadLine() == "sí";
+Console.Clear();
+
+ while (Empezar) ;
+
+Console.WriteLine("¡Gracias por jugar!");
+{
+
+    
+
+    
+  
+}
+
